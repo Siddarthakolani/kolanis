87bdec6667d7207bae7e4693060320b3946c2075
Siddarthakolani <85723896+Siddarthakolani@users.noreply.github.com>
2021-07-19 21:07:34 -0700

    Delete FrontEnd directory

--- FrontEnd/LoanProccessingSystem-Scrumteam-5-master/LoanProccessingSystem/src/main/java/com/loan/app/service/LoanVerificationManagerService.java
+++
@@ -1,9 +0,0 @@
-package com.loan.app.service;
-
-import com.loan.app.entities.LoanApplication;
-import com.loan.app.exception.InvalidLoanApplicationException;
-
-public interface LoanVerificationManagerService {
-
-	public LoanApplication updateStatus(LoanApplication loanApplication) throws InvalidLoanApplicationException ;
-}
